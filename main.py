1ac56de7ab76a927a8515b8d6ecbe2a9a19b48f9
Zeebra38 <darios2013@bk.ru>
2021-06-07 21:36:44 +0300

    Сделано. Есть возмозможность ввести название с клавиатуры или вставить ссылку на страницу с трофеями

--- main.py
+++ main.py
@@ -1,70 +1,117 @@
+# region Imports and defines
 import time
 import re
-import bs4
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from config import CHROMEDRIVER_PATH #Путь к ChromeDriver
+from config import CHROMEDRIVER_PATH  # Путь к ChromeDriver
 from bs4 import BeautifulSoup
 from trophie import Trophie, guide
-rarity_pattern = re.compile(r'\d{1,3}[.]\d{1,2}') #паттерн для поиска процентов игроков
-borders = { # ключ - класс рамки, значение - Описание на русском
+import os
+
+rarity_pattern = re.compile(r'\d{1,3}\.\d{1,2}')  # паттерн для поиска процентов игроков
+link_match_with_ajax = re.compile(r'^https:\/\/www\.stratege\.ru\/ps4\/games\/.+\/trophies#args:ajax=1$')
+link_match_without_ajax = re.compile(r'^https:\/\/www\.stratege\.ru\/ps4\/games\/.+\/trophies$')
+borders = {  # ключ - класс рамки, значение - Описание на русском
     'tltstpl_border_platinum_trophies': 'Платиновый',
     'tltstpl_border_gold_trophies': 'Золотой',
     'tltstpl_border_silver_trophies': 'Серебряный',
     'tltstpl_border_bronze_trophies': 'Бронзовый'
 }
 trophy_list = []
-# region Opening Browser
-# options = Options()
-# options.headless = True
-# browser = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
-# browser.get('https://www.stratege.ru/ps4/games/hollow_knight/trophies#args:ajax=1')
-# time.sleep(5)  # т.к. страница медленно грузится
-# page = browser.page_source
-# soup = BeautifulSoup(page, 'lxml')
-# browser.close()
-# with open('index.html', 'w') as f:
-#     f.write(str(soup))
-# endregion
-# region Parsing html
-with open('index.html', 'r') as f:
-    soup = BeautifulSoup(f, 'lxml')
-for trophie in soup.find_all(attrs={'class': 'tltstpl_trophies'}):
-    title = trophie.find(attrs={'class': 'tltstpl_tt_trops_title_box'}).text.strip()
-    tag = trophie.find(attrs={'class': 'tltstpl_border_trophies'})
-    difficult = borders[tag['class'][1]]
-    rarity = float(rarity_pattern.search(trophie.find(attrs={'class': 'tltstpl_tt_trops_rarity'}).text.strip()).group(0))
-    for slider_page in help_list.find_all(attrs={'class':'tlhsltpl_slider_table_td'}):
-        help_list = trophie.find(attrs={'class': 'tlhsltpl_helps_body'})
-        for guide in help_list.find_all(attrs={'class':'tlhsltpl_helps_info_helps'}):
-            author_tag = guide.find(attrs={'class':'tlhsltpl_helps_header_autor'})
-            author = str.join(' ', [el for el in author_tag.find('a')['title'].split()[1:]])
-            help_info = guide.find(attrs={'class': 'viewstgix_layer_find'})
-            rez = help_info.text.strip('\t')
-            for link in help_info.find_all('a'):
-                rez += link.get('href')
-            print(rez)
-        for guide_add in help_list.find_all(attrs={'class':'tlhsltpl_add_helps_header'}):
-            author_tag = guide_add.find(attrs={'class': 'tlhsltpl_helps_header_autor'})
-            author = str.join(' ', [el for el in author_tag.find('a')['title'].split()[1:]])
-            help_info = guide_add.find(attrs={'class': 'viewstgix_layer_find'})
-            rez = help_info.text.strip('\t')
-            for link in help_info.find_all('a'):
-                rez += link.get('href')
-            print(rez)
 
 
-        #tlhsltpl_helps_text_helps viewstgix_layer_find
-        #tlhsltpl_add_helps_text viewstgix_layer_find
-        # for child in author_tag.children:
-        #     print(type(child))
-        #     print(child)
-            #print(child.attrs)
-            # if 'Пользователь' in child.title:
-            #     print(child.title)
-        break
-    trophy_list.append(Trophie(title, difficult, rarity))
-    #print(f'{title}\n{difficult}')
-# print(str.join('', trophy_list))x
-#print(trophy_list)
 # endregion
+
+def get_trophies(game='', out_link=''):
+    # region Opening Browser
+    link = "https://www.stratege.ru/ps4/games/{game}/trophies#args:ajax=1"  # ссылка, куда будем подставлять название игры
+    options = Options()
+    options.headless = True
+    browser = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
+    if out_link == '':
+        browser.get(link.format(game=game))
+    else:
+        browser.get(out_link)
+    time.sleep(5)  # т.к. страница медленно грузится
+    page = browser.page_source
+    browser.close()
+    soup = BeautifulSoup(page, 'lxml')
+    if 'К сожалению, такой игры на сайте не существует.' in soup.text:
+        print('К сожалению, такой игры на сайте не существует. Попробуйте снова')
+        return -1
+    # endregion
+    # region Parsing html
+    for trophie in soup.find_all(attrs={'class': 'tltstpl_trophies'}):
+        title = trophie.find(attrs={'class': 'tltstpl_tt_trops_title_box'}).text.strip()
+        tag = trophie.find(attrs={'class': 'tltstpl_border_trophies'})
+        difficult = borders[tag['class'][1]]
+        rarity = float(
+            rarity_pattern.search(trophie.find(attrs={'class': 'tltstpl_tt_trops_rarity'}).text.strip()).group(0))
+        guides = []
+        for slider_page in trophie.find_all(attrs={'class': 'tlhsltpl_slider_table_td'}):
+            main_part = {}
+            additions = {}
+            index = 0
+            for author_tag, guide_tag in zip(slider_page.find_all(attrs={'class': 'tlhsltpl_helps_header_autor'}),
+                                             slider_page.find_all(attrs={'class': 'viewstgix_layer_find'})):
+                author = str.join(' ', [el for el in author_tag.find('a')['title'].split()[1:]])
+                guide_data = guide_tag.text.strip()
+                for link in guide_tag.find_all('a'):
+                    guide_data += '\n' + link.get('href')
+                if index == 0:
+                    main_part[author] = guide_data
+                else:
+                    additions[author] = guide_data
+                index += 1
+            guides.append(guide(main_part, additions))
+        trophy_list.append(Trophie(title, difficult, rarity, guides))
+    return 0
+    # endregion
+
+
+def main_menu():
+    while True:
+        try:
+            print(
+                'Введите название игры с трофеями для PS4, \nпришлите ссылку на страницу с трофеями сайта stratege.ru \nили напишите "Выход" для выхода из программы. \nStratege.ru работает очень медленно!')
+            choice = input()
+            if link_match_with_ajax.match(choice) or link_match_without_ajax.match(choice):
+                if get_trophies(choice) == 0:
+                    choice2 = input('Вывести подсказки по всем трофеям? Да\нет\n')
+                    if choice2.lower() == 'да':
+                        print(str.join('', [str(el) for el in trophy_list]))
+                    elif choice2.lower() == 'нет':
+                        print(str.join('\n', [str(el.title) for el in trophy_list]))
+                        choice3 = input('Введите название трофея из списка\n')
+                        if choice3 in [troph.title for troph in trophy_list]:
+                            print([troph for troph in trophy_list if troph.title == choice3][0])
+                        else:
+                            print(f'Такого трофея нет. Введите название полностью. Например "{trophy_list[0].title}"')
+                    else:
+                        print('Я вас не понимаю :(')
+            elif choice.lower().strip() == 'выход':
+                print("Пока!")
+                exit(0)
+            else:
+                if get_trophies(str.join('_', choice.split())) == 0:
+                    choice2 = input('Вывести подсказки по всем трофеям? Да\нет\n')
+                    if choice2.lower() == 'да':
+                        print(str.join('', [str(el) for el in trophy_list]))
+                    elif choice2.lower() == 'нет':
+                        print(str.join('\n', [str(el.title) for el in trophy_list]))
+                        choice3 = input('Введите название трофея из списка\n')
+                        if choice3 in [troph.title for troph in trophy_list]:
+                            print([troph for troph in trophy_list if troph.title == choice3][0])
+                        else:
+                            print(f'Такого трофея нет. Введите название полностью. Например "{trophy_list[0].title}"')
+                    else:
+                        print('Я вас не понимаю :(')
+            input()
+            os.system('cls')
+            trophy_list.clear()
+        except Exception as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    main_menu()
--- trophie.py
+++ trophie.py
@@ -1,37 +1,36 @@
-class Trophie:
-    def __init__(self, title: str, difficult: str, rarity: float, help_info: dict = {}):
-        self.title = title
-        self.difficult = difficult
-        self.rarity = rarity
-        self.help_info = help_info
-
-    def __str__(self):
-        rez = ''
-        if len(self.help_info) != 0:
-            for item in self.help_info.items():
-                rez += f'Подказка от {item[0]}:\n{item[1]}\n'
-        return f'{self.difficult} трофей {self.title}.\nЕсть у {self.rarity} игроков.\n{rez if rez != "" else "Подсказок нет"}'
-
-    def __repr__(self):
-        rez = ''
-        if len(self.help_info) != 0:
-            for item in self.help_info.items():
-                rez += f'Подказка от {item[0]}:\n{item[1]}\n'
-        return f'{self.difficult} трофей {self.title}.\nЕсть у {self.rarity} игроков.\n{rez if rez != "" else "Подсказок нет"}'
-
 class guide:
-    def __init__(self, main_part: dict, additions: dict):
+    def __init__(self, main_part: dict = {}, additions: dict = {}):
         self.main_part = main_part
         self.additions = additions
 
     def __str__(self):
-        rez = ''
-        for key, value in self.additions.items():
-            rez += f'Дополнение от {key}: \n{value}'
-        return f'Подсказка от {list(self.main_part.keys())[0]}: \n{self.main_part[list(self.main_part.keys())[0]]}.\nДополнения к подсказке:{rez} '
+        if len(self.additions.items()) > 0:
+            rez = ''
+            for key, value in self.additions.items():
+                rez += f'\nДополнение от {key}: \n{value.strip()}\n'
+            return f'Подсказка от {list(self.main_part.keys())[0]}: \n{self.main_part[list(self.main_part.keys())[0]]}.\nДополнения к подсказке:{rez}'
+        else:
+            return 'Подсказок нет'
 
     def __repr__(self):
-        rez = ''
-        for key, value in self.additions.items():
-            rez += f'Дополнение от {key}: \n{value}'
-        return f'Подсказка от {list(self.main_part.keys())[0]}: \n{self.main_part[list(self.main_part.keys())[0]]}.\nДополнения к подсказке:{rez} '
+        if len(self.additions.items()) > 0:
+            rez = ''
+            for key, value in self.additions.items():
+                rez += f'\nДополнение от {key}: \n{value.strip()}\n'
+            return f'Подсказка от {list(self.main_part.keys())[0]}: \n{self.main_part[list(self.main_part.keys())[0]]}.\nДополнения к подсказке:{rez}'
+        else:
+            return 'Подсказок нет'
+
+
+class Trophie:
+    def __init__(self, title: str, difficult: str, rarity: float, cur_guide: list[guide]):
+        self.title = title
+        self.difficult = difficult
+        self.rarity = rarity
+        self.cur_guide = cur_guide
+
+    def __str__(self):
+        return f'{self.difficult} трофей {self.title}.\nЕсть у {self.rarity}% игроков.\n{str.join(" ", [str(el) for el in self.cur_guide])}\n'
+
+    def __repr__(self):
+        return f'{self.difficult} трофей {self.title}.\nЕсть у {self.rarity}% игроков.\n{str.join(" ", [str(el) for el in self.cur_guide])}\n'
